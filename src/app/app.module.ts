27cb4106c8928b28c587590fb45f96a9a02333b7
Marcin Matejko <matejko.tsp@gmail.com>
2019-11-27 13:18:50 +0100

    zamiana caruzeli na cardy ze zdjeciami

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CarouselComponent
+    CarouselComponent,
+    AboutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
