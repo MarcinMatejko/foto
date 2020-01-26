6ef1f5e8d522489c6f2e88965e17d38dc64503d2
Marcin Matejko <matejko.tsp@gmail.com>
2020-01-26 23:41:56 +0100

    dodanie meta tagow, przyspieszenie animacji o polowe

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -8,32 +8,32 @@ import {
 
 export let fadeRight = trigger("fadeRight", [
   state("void", style({ opacity: 0, transform: "translateX(-60px)" })),
-  transition("void => *", [animate("1200ms 1.2s")])
+  transition("void => *", [animate("600ms 0.6s")])
 ]);
 
 export let fadeLeft = trigger("fadeLeft", [
   state("void", style({ opacity: 0, transform: "translateX(60px)" })),
-  transition("void => *", [animate("1200ms 1.8s")])
+  transition("void => *", [animate("600ms 0.9s")])
 ]);
 
 export let fadeUp = trigger("fadeUp", [
   state("void", style({ opacity: 0, transform: "translateY(100px)" })),
-  transition("void => *", [animate("1200ms 1.2s")])
+  transition("void => *", [animate("600ms 0.6s")])
 ]);
 export let fadeUp1 = trigger("fadeUp1", [
   state("void", style({ opacity: 0, transform: "translateY(100px)" })),
-  transition("void => *", [animate("1200ms 1.7s")])
+  transition("void => *", [animate("600ms 0.85s")])
 ]);
 export let fadeUp2 = trigger("fadeUp2", [
   state("void", style({ opacity: 0, transform: "translateY(100px)" })),
-  transition("void => *", [animate("1200ms 2.2s")])
+  transition("void => *", [animate("600ms 1.1s")])
 ]);
 
 export let fadeDown = trigger("fadeDown", [
   state("void", style({ opacity: 0, transform: "translateY(-40px)" })),
-  transition("void => *", [animate("1200ms 0.6s")])
+  transition("void => *", [animate("600ms 0.3s")])
 ]);
 export let fadeDown2 = trigger("fadeDown2", [
   state("void", style({ opacity: 0, transform: "translateY(-40px)" })),
-  transition("void => *", [animate("1200ms 0.9s")])
+  transition("void => *", [animate("600ms 0.45s")])
 ]);
